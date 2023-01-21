c7f5cfd97f31799eaf1e357f4ad19aa93c5affab
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2023-01-21 13:24:49 +0100

    Updated 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,49 +1,31 @@
-import * as React from "react"
-import { Link, HeadFC } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import React from "react"
+import { Link } from "gatsby"
+import { useLottie } from "lottie-react";
+// @ts-ignore
+import { Layout } from "../components/Layout.js"
+// @ts-ignore
+import errorAnimation from "../images/404.json";
 
 const NotFoundPage = () => {
+  const options = {
+    animationData: errorAnimation,
+    loop: true
+  };
+
+  const { View } = useLottie(options);
+
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+      <Layout>
+      <main className="flex flex-1 flex-col justify-center items-center px-5.5 text-center">
+        <div className="max-w-screen-sm w-full mb-10">
+        {View}
+        </div>
+        <h1 className="sr-only">Sidan kan inte hittas</h1>
+        <p className="font-serif text-xl text-primary pb-4">Åh nej! Sidan kan inte hittas.</p>
+        <Link to="/" className="font-sans font-semibold text-xs uppercase tracking-wider px-8 py-2 rounded-full text-white bg-primary hover:bg-primary/70 transition-all">Tillbaka till start</Link>
+      </main>
+      </Layout>
   )
 }
 
 export default NotFoundPage
-
-export const Head: HeadFC = () => <title>Not found</title>
