914aadd1684d7d44230ac2f93a93e19a1e2bd303
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2023-02-10 22:53:39 +0100

    Fixed overhanging images

--- src/components/cards/RecipeCard.js
+++ src/components/cards/RecipeCard.js
@@ -9,7 +9,7 @@ export default function RecipeCard({recipe}) {
    return (
       <Link to={recipe.url} className="w-full">
          <figure className="relative w-full aspect-w-16 aspect-h-9 rounded-xl overflow-hidden">
-         <GatsbyImage image={image} className="absolute inset-0 w-full h-full rounded-xl overflow-hidden" imgClassName="rounded-xl" alt={recipe.data.preview.alt}/>
+         <GatsbyImage image={image} style={{position: 'absolute'}} className="absolute inset-0 w-full h-full rounded-xl overflow-hidden" imgClassName="rounded-xl" alt={recipe.data.preview.alt}/>
          </figure>
          <div className="mt-3">
             <h2 className="font-serif font-bold text-base leading-normal text-primary"><PrismicText field={recipe.data.title.richText} /></h2>
